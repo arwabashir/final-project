f7ce7df70f8e1bcde1624a9b1e41f1da11965536
HanaHansen <hanahansen@gmail.com>
2024-04-24 12:07:13 -0500

    updaing app.js with main changes

--- public/app.js
+++ public/app.js
@@ -299,17 +299,6 @@ function attachBookingListeners() {
   });
 }
 
-function attachAddListeners() {
-  const addButtons = document.querySelectorAll(".add-btn");
-  addButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const card = button.closest(".card");
-      const date = card.id;
-      showAddModal(date);
-    });
-  });
-}
-
 // Sets the default drop down month to the current month and default month on page to the curent month and year
 document.addEventListener("DOMContentLoaded", function () {
   const today = new Date();
